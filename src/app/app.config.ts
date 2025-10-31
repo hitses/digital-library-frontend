e1989373794fc490e11a00fa72e7ac6610af5368
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-31 10:22:25 +0100

    A - Global configs on app.config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withIncrementalHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withIncrementalHydration(), withEventReplay()),
+  ],
 };
