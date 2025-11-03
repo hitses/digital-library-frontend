4bb6568a74af1441d1caf0aaad0f55d3aaac0cf0
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-03 11:58:38 +0100

    U - Books Home show featured, new & popular Books

--- src/app/features/books/pages/home/home.ts
+++ src/app/features/books/pages/home/home.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { BookList } from '../../components/book-list/book-list';
+import { IBook } from '../../models/book.interface';
+import { BookService } from '../../services/book';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BookList],
   templateUrl: './home.html',
   styles: ``,
 })
-export default class Home {}
+export default class Home implements OnInit {
+  books: WritableSignal<IBook[]> = signal<IBook[]>([]);
+
+  protected readonly bookService = inject(BookService);
+
+  featuredBooks = this.bookService.featuredBooks;
+  newBooks = this.bookService.newBooks;
+  popularBooks = this.bookService.popularBooks;
+
+  ngOnInit(): void {
+    this.bookService.getFeaturedBooks();
+    this.bookService.getNewBooks();
+    this.bookService.getPopularBooks();
+  }
+}
