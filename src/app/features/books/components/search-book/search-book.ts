4a15fdcf3f20e5b541d5e3445d7076ef2faaf117
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-03 11:51:03 +0100

    A - Search Book component

---
+++ src/app/features/books/components/search-book/search-book.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-book-component',
+  imports: [],
+  templateUrl: './search-book.html',
+  styles: ``,
+})
+export class SearchBook {}
