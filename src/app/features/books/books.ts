df03bd343b330b8408278a3df80ea81a01a8df82
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-03 11:51:50 +0100

    U - Header component in Book component

--- src/app/features/books/books.ts
+++ src/app/features/books/books.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-books',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './books.html',
   styles: ``,
 })
