008e881e229305c7e4c1b4f109af0bed8e62d042
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-04 15:31:30 +0100

    A - Book Detail Component

---
+++ src/app/features/books/pages/detail/detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'detail-page',
+  imports: [],
+  templateUrl: './detail.html',
+  styles: ``,
+})
+export default class Detail {}
