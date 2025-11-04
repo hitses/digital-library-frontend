382b36f19f75131ccd2e6fa23f98020f0508d75d
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-04 15:27:53 +0100

    A - Router Link to Home in Header

--- src/app/features/books/components/header/header.ts
+++ src/app/features/books/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SearchBook } from '../search-book/search-book';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'books-header-component',
-  imports: [SearchBook],
+  imports: [SearchBook, RouterLink],
   templateUrl: './header.html',
   styles: ``,
 })
