1d96a83f561ea6808d460bf97db604cb002bb34b
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-04 13:15:15 +0100

    U - Get API URL from environment variable

--- src/app/features/books/services/book.ts
+++ src/app/features/books/services/book.ts
@@ -1,12 +1,13 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IBook } from '../models/book.interface';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private readonly apiUrl = 'http://localhost:3000/api/book';
+  private readonly apiUrl = environment.apiUrl + '/book';
 
   featuredBooks = signal<IBook[]>([]);
   newBooks = signal<IBook[]>([]);
@@ -17,21 +18,30 @@ export class BookService {
   getFeaturedBooks(): void {
     this.http.get<IBook[]>(`${this.apiUrl}/featured`).subscribe({
       next: (books) => this.featuredBooks.set(books),
-      error: (err) => this.featuredBooks.set([]),
+      error: (err) => {
+        console.error(err);
+        return this.featuredBooks.set([]);
+      },
     });
   }
 
   getNewBooks(): void {
     this.http.get<IBook[]>(`${this.apiUrl}/new`).subscribe({
       next: (books) => this.newBooks.set(books),
-      error: (err) => this.newBooks.set([]),
+      error: (err) => {
+        console.error(err);
+        return this.newBooks.set([]);
+      },
     });
   }
 
   getPopularBooks(): void {
     this.http.get<IBook[]>(`${this.apiUrl}/popular`).subscribe({
       next: (books) => this.popularBooks.set(books),
-      error: (err) => this.popularBooks.set([]),
+      error: (err) => {
+        console.error(err);
+        return this.popularBooks.set([]);
+      },
     });
   }
 }
