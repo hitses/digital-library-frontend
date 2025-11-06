902f16815b2d8c006d92857c238a3210e896b2cf
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-07 00:02:20 +0100

    A - Create Review in service

--- src/app/features/review/service/review.ts
+++ src/app/features/review/service/review.ts
@@ -1,16 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { IReviews } from '../models/review.interface';
+import { IReview, IReviews } from '../models/review.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReviewService {
   private readonly http = inject(HttpClient);
-  private readonly baseUrl = environment.apiUrl + '/review';
+  private readonly baseUrl = environment.apiUrl;
 
   reviews = signal<IReviews | null>(null);
+
+  createReview(bookId: string, name: string, email: string, review: string, rating: number) {
+    return this.http.post<IReview>(`${this.baseUrl}/review`, {
+      bookId,
+      name,
+      email,
+      review,
+      rating,
+    });
+  }
 
   getReviewsByBook(bookId: string, page = 1): void {
     this.http.get<IReviews>(`${this.baseUrl}/book/${bookId}?page=${page}`).subscribe({
