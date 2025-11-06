345de52a432c2e69175bb654e609bb9df7cbc7ad
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-06 12:15:57 +0100

    U - Find one Book redirects to Detail Page

--- src/app/features/books/pages/search/search.ts
+++ src/app/features/books/pages/search/search.ts
@@ -1,12 +1,12 @@
 import { Component, effect, inject, input, OnDestroy } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { BookService } from '../../services/book';
 import { BookList } from '../../components/book-list/book-list';
 import { Pagination } from '../../../../core/components/pagination/pagination';
 
 @Component({
   selector: 'search-page',
-  imports: [RouterLink, BookList, Pagination],
+  imports: [BookList, Pagination],
   templateUrl: './search.html',
   styles: ``,
 })
@@ -14,6 +14,7 @@ export default class Search implements OnDestroy {
   q = input<string>('');
 
   protected readonly bookService = inject(BookService);
+  private readonly router = inject(Router);
 
   searchResults = this.bookService.searchResults;
   currentPage = 1;
@@ -22,37 +23,30 @@ export default class Search implements OnDestroy {
     effect(() => {
       const query = this.q();
 
-      if (query || query === '') this.bookService.searchBooks(query, this.currentPage);
+      this.bookService.clearBook();
+      this.currentPage = 1;
+      this.bookService.searchBooks(query, this.currentPage);
     });
-  }
 
-  getPagesArray(totalPages: number): number[] {
-    return Array.from({ length: totalPages }, (_, i) => i + 1);
+    effect(() => {
+      const results = this.searchResults();
+      const query = this.q();
+
+      if (results && results.total === 1 && query !== '') {
+        const id = results.data[0]._id;
+        this.router.navigate(['/', id]);
+      }
+    });
   }
 
   goToPage(page: number): void {
     this.currentPage = page;
-
     const query = this.q();
 
-    if (query || query === '') this.bookService.searchBooks(query, page);
+    this.bookService.searchBooks(query, page);
   }
 
   ngOnDestroy(): void {
     this.bookService.clearSearchResults();
-  }
-
-  getMiddlePages(): number[] {
-    const current = this.searchResults()!.page;
-    const total = this.searchResults()!.totalPages;
-
-    const pages = [];
-
-    const start = Math.max(2, current - 1);
-    const end = Math.min(total - 1, current + 1);
-
-    for (let p = start; p <= end; p++) pages.push(p);
-
-    return pages;
   }
 }
