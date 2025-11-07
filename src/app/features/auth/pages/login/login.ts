b0c03f1fe799571f9c2b59b576dd0f3c5c262bc7
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-07 23:46:42 +0100

    A. -Login component

---
+++ src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,53 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './login.html',
+  styles: ``,
+})
+export default class Login {
+  loading = signal(false);
+  error = signal('');
+
+  private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+
+  loginForm = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  get email() {
+    return this.loginForm.get('email') as FormControl;
+  }
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  async onSubmit() {
+    if (this.loginForm.invalid) {
+      this.email.markAsTouched();
+      this.password.markAsTouched();
+      return;
+    }
+
+    this.loading.set(true);
+    this.error.set('');
+
+    await new Promise((r) => setTimeout(r, 900));
+
+    const email = this.email.value;
+    const pass = this.password.value;
+
+    if (email === 'demo@demo.test' && pass === 'demo123') {
+      this.router.navigateByUrl('/');
+    } else {
+      this.error.set('Credenciales inválidas');
+    }
+
+    this.loading.set(false);
+  }
+}
