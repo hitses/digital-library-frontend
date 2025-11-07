6f6da5d0cdf1feebaaf8221fdb31015aa32abfae
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-07 23:46:05 +0100

    A - Login routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./features/books/books.routes').then((r) => r.bookRoutes),
-  },
-  {
     path: 'dash',
     loadChildren: () => import('./features/dash/dash.routes').then((r) => r.dashRoutes),
-    // canActivate: [AuthGuard],
   },
   {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then((r) => r.authRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./features/books/books.routes').then((r) => r.bookRoutes),
   },
   { path: '**', redirectTo: '' },
 ];
--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const authRoutes: Routes = [{ path: '**', redirectTo: '' }];
+export const authRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./auth'),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login'),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
