a04dac976470d75341f04c01910cf9e4012447f6
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-09 18:54:58 +0100

    A - Sun & Moon icons

---
+++ src/app/core/icons/moon/moon.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'moon-icon',
+  imports: [],
+  templateUrl: './moon.html',
+  styles: ``,
+})
+export class Moon {}
---
+++ src/app/core/icons/sun/sun.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sun-icon',
+  imports: [],
+  templateUrl: './sun.html',
+  styles: ``,
+})
+export class Sun {}
