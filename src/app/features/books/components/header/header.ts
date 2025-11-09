52004405480f727e9fe2af464add56c58cfa251e
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-09 18:56:42 +0100

    A - Style details & light/dark switch

--- src/app/features/books/components/header/header.ts
+++ src/app/features/books/components/header/header.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SearchBook } from '../search-book/search-book';
 import { RouterLink } from '@angular/router';
+import { Moon } from '../../../../core/icons/moon/moon';
+import { Sun } from '../../../../core/icons/sun/sun';
 
 @Component({
   selector: 'books-header-component',
-  imports: [SearchBook, RouterLink],
+  imports: [SearchBook, RouterLink, Moon, Sun],
   templateUrl: './header.html',
   styles: ``,
 })
-export class Header {}
+export class Header {
+  private readonly STORAGE_KEY = 'theme';
+
+  theme = signal<'light' | 'dark'>('light');
+
+  constructor() {
+    const saved = localStorage.getItem(this.STORAGE_KEY) as 'light' | 'dark' | null;
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    this.theme.set(saved ?? (prefersDark ? 'dark' : 'light'));
+    this.applyTheme();
+  }
+
+  toggle() {
+    const next = this.theme() === 'light' ? 'dark' : 'light';
+
+    this.theme.set(next);
+
+    localStorage.setItem(this.STORAGE_KEY, next);
+
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    const htmlEl = document.documentElement;
+
+    if (this.theme() === 'dark') htmlEl.classList.add('dark');
+    else htmlEl.classList.remove('dark');
+  }
+}
