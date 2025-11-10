2a0c319ea75148e98d4e0d35865b1307545ab962
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-10 14:30:12 +0100

    A - Forgot componente & functionality

---
+++ src/app/features/auth/pages/forgot/forgot.ts
@@ -0,0 +1,45 @@
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ForgotService } from '../../services/forgot';
+import { IForgotResponse } from '../../models/forgotResponse.interface';
+
+@Component({
+  selector: 'app-forgot',
+  imports: [ReactiveFormsModule],
+  templateUrl: './forgot.html',
+  styles: ``,
+})
+export default class Forgot {
+  loading = signal(false);
+
+  private readonly fb = inject(FormBuilder);
+  private readonly forgotService = inject(ForgotService);
+
+  forgotResponse: WritableSignal<IForgotResponse | null> = this.forgotService.forgotResponse;
+  error: WritableSignal<string | number> = this.forgotService.error;
+
+  forgotForm = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  get email() {
+    return this.forgotForm.get('email') as FormControl;
+  }
+
+  onSubmit() {
+    if (this.forgotForm.invalid) {
+      this.email.markAsTouched();
+
+      return;
+    }
+
+    this.loading.set(true);
+    this.error.set('');
+
+    const email = this.email.value;
+
+    this.forgotService.forgotPassword(email!);
+
+    this.loading.set(false);
+  }
+}
