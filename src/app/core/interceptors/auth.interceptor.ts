c27d92b2ee830a0230801bcc88131690081cc116
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 09:38:23 +0100

    U - Auth interceptor redirect if response 401 or 403

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,19 +1,31 @@
-import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpHandlerFn,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<any>,
   next: HttpHandlerFn,
 ): Observable<HttpEvent<any>> => {
   const token = localStorage.getItem('token');
+  const router = inject(Router);
 
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` },
-    });
+  const cloned = token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req;
 
-    return next(cloned);
-  }
-
-  return next(req);
+  return next(cloned).pipe(
+    tap({
+      error: (err) => {
+        if (err instanceof HttpErrorResponse && (err.status === 401 || err.status === 403)) {
+          localStorage.removeItem('token');
+          router.navigate(['/']);
+        }
+      },
+    }),
+  );
 };
