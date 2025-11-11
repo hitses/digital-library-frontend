69d2ad95a5be316fb3279de9feba31869eb0f6c9
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 14:56:30 +0100

    U - Dashboard children routes

--- src/app/features/dash/dash.routes.ts
+++ src/app/features/dash/dash.routes.ts
@@ -4,7 +4,28 @@ export const dashRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./dash'),
-    children: [],
+    children: [
+      {
+        path: '',
+        title: 'Dashboard | Inicio',
+        loadComponent: () => import('./pages/home/home'),
+      },
+      {
+        path: 'books',
+        title: 'Dashboard | Libros',
+        loadComponent: () => import('./pages/books/books'),
+      },
+      {
+        path: 'reviews',
+        title: 'Dashboard | Reseñas',
+        loadComponent: () => import('./pages/reviews/reviews'),
+      },
+      {
+        path: 'settings',
+        title: 'Dashboard | Ajustes',
+        loadComponent: () => import('./pages/settings/settings'),
+      },
+    ],
   },
-  { path: '**', redirectTo: 'login' },
+  { path: '**', redirectTo: '' },
 ];
