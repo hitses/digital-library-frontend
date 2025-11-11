2c8869bbe1e04da5c5e16e508b0119646b10be4a
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 14:34:42 +0100

    A - Get total Books & Reviews

--- src/app/features/dash/dash.ts
+++ src/app/features/dash/dash.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { ResumeCard } from './components/resume-card/resume-card';
+import { DashService } from './services/dash';
 
 @Component({
   selector: 'app-dash',
-  imports: [Header, Footer],
+  imports: [Header, Footer, ResumeCard],
   templateUrl: './dash.html',
   styles: ``,
 })
-export default class Dash {}
+export default class Dash {
+  private readonly dashService = inject(DashService);
+
+  resumeData = computed(() => [
+    { title: 'Total de libros', metric: this.dashService.totalBooks() },
+    { title: 'Reseñas publicadas', metric: this.dashService.totalReviews() },
+    { title: 'Reseñas pendientes', metric: 532 },
+  ]);
+
+  constructor() {
+    this.dashService.getTotalBooks();
+    this.dashService.getTotalReviews();
+  }
+}
