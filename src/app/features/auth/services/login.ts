848467d47983850381410ba28363a9788fa65541
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 10:20:11 +0100

    A - Login API call

--- src/app/features/auth/services/login.ts
+++ src/app/features/auth/services/login.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LoginService {}
+export class LoginService {
+  private readonly baseUrl = `${environment.apiUrl}/auth`;
+
+  private readonly http = inject(HttpClient);
+
+  login(email: string, password: string) {
+    return this.http.post<{ id: string; email: string; token: string }>(`${this.baseUrl}/login`, {
+      email,
+      password,
+    });
+  }
+}
