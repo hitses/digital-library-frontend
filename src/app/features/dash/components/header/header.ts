36783ee3615df2ade6e92f9498aa9c3bd25d00cc
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 11:54:20 +0100

    A - Dashboard Header

---
+++ src/app/features/dash/components/header/header.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'dash-header-component',
+  imports: [RouterLink],
+  templateUrl: './header.html',
+  styles: ``,
+})
+export class Header {
+  navLinks = [
+    {
+      label: 'Inicio',
+      link: '/dash',
+    },
+    {
+      label: 'Libros',
+      link: '/dash/books',
+    },
+    {
+      label: 'Reseñas',
+      link: '/dash/reviews',
+    },
+    {
+      label: 'Ajustes',
+      link: '/dash/settings',
+    },
+  ];
+
+  private readonly router = inject(Router);
+
+  logout() {
+    localStorage.removeItem('token');
+
+    this.router.navigate(['/']);
+  }
+}
