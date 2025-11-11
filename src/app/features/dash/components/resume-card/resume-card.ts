500d8eb493596420ba9c0690939e8597294230e9
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 14:34:31 +0100

    A - Dashboard Info Card

---
+++ src/app/features/dash/components/resume-card/resume-card.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'dash-resume-card-component',
+  imports: [],
+  templateUrl: './resume-card.html',
+  styles: ``,
+})
+export class ResumeCard {
+  title = input.required<string>();
+  metric = input.required<number>();
+}
