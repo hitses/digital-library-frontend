78adf6d128dc449b05ad7c11b8404ca34052d35f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 10:17:41 +0100

    A - Eyes icons

---
+++ src/app/core/icons/closed-eye/closed-eye.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'closed-eye-icon',
+  imports: [],
+  templateUrl: './closed-eye.html',
+  styles: ``,
+})
+export class ClosedEye {}
---
+++ src/app/core/icons/eye/eye.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eye-icon',
+  imports: [],
+  templateUrl: './eye.html',
+  styles: ``,
+})
+export class Eye {}
