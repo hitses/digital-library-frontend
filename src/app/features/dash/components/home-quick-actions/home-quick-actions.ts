7415272babaf84885f41aa02879290bebb870049
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-11 15:50:10 +0100

    A - Dashboard Home Quick Actions component

---
+++ src/app/features/dash/components/home-quick-actions/home-quick-actions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dash-home-quick-actions-component',
+  imports: [],
+  templateUrl: './home-quick-actions.html',
+  styles: ``,
+})
+export class HomeQuickActions {}
--- src/app/features/dash/pages/home/home.ts
+++ src/app/features/dash/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject } from '@angular/core';
 import { DashService } from '../../services/dash';
 import { HomeResume } from '../../components/home-resume/home-resume';
+import { HomeQuickActions } from '../../components/home-quick-actions/home-quick-actions';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeResume],
+  imports: [HomeResume, HomeQuickActions],
   templateUrl: './home.html',
   styles: ``,
 })
