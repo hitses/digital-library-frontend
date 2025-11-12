009f7f8212540a027e2501e8449218aa8b6e06f8
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-12 12:14:39 +0100

    U - Hashed data to DB data & logic

--- src/app/features/dash/pages/books/books.ts
+++ src/app/features/dash/pages/books/books.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ResumeCard } from '../../components/home/resume-card/resume-card';
+import { BooksService } from '../../services/books';
 
 @Component({
   selector: 'app-books',
@@ -8,38 +9,27 @@ import { ResumeCard } from '../../components/home/resume-card/resume-card';
   styles: ``,
 })
 export default class Books {
+  books = computed(() => this.booksService.books());
+  totalBooksCount = computed(() => this.booksService.totalBooks());
+  featuredBooksCount = computed(() => this.booksService.featuredBooks().length);
+  reviewlessBooksCount = computed(() => this.booksService.reviewlessBooks().length);
+  totalReviewlessBooksCount = computed(() => this.booksService.totalReviewlessBooks());
+  recentBooks = computed(() => this.booksService.recentBooks());
+
+  private readonly booksService = inject(BooksService);
+
+  constructor() {
+    this.booksService.getTotalBooks();
+    this.booksService.getFeaturedBooks();
+    this.booksService.getReviewlessBooks();
+    this.booksService.getRecentBooks();
+  }
+
   stats = {
     totalBooks: 128,
     featuredBooks: 9,
     avgRating: 4.2,
   };
-
-  books = [
-    {
-      _id: '1',
-      title: 'El silencio de la ciudad blanca',
-      author: 'Eva García Sáenz de Urturi',
-      isbn: '9788491290664',
-      coverUrl: 'https://m.media-amazon.com/images/I/81f93vY2T-L._AC_UF1000,1000_QL80_.jpg',
-      featured: true,
-    },
-    {
-      _id: '2',
-      title: 'Los pilares de la Tierra',
-      author: 'Ken Follett',
-      isbn: '9780451225245',
-      coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/91t6yVjcp-L.jpg',
-      featured: false,
-    },
-    {
-      _id: '3',
-      title: 'Cien años de soledad',
-      author: 'Gabriel García Márquez',
-      isbn: '9780307474728',
-      coverUrl: 'https://images.penguinrandomhouse.com/cover/9780525562443',
-      featured: false,
-    },
-  ];
 
   editBook(bookId: string) {}
 }
