3e01c324ca7777f411d574483a8d2ecb87a00aa5
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-14 19:51:25 +0100

    U - Link to Book details in Dashboard

--- src/app/features/dash/components/home/activity/activity.ts
+++ src/app/features/dash/components/home/activity/activity.ts
@@ -1,10 +1,11 @@
 import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
 import '@github/relative-time-element';
 import { HomeService } from '../../../services/home';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'activity-component',
-  imports: [],
+  imports: [RouterLink],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './activity.html',
   styles: ``,
