f65d62eb5346c9aa3b1b8dcd01e58400bfd4ca8f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-18 13:06:15 +0100

    A - Confirm Dialog in App component globally

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastContainerComponent } from './core/components/toast-container/toast-container.component';
+import { ConfirmDialog } from './core/components/confirm-dialog/confirm-dialog';
+import { ConfirmDialogService } from './core/services/confirm-dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastContainerComponent],
+  imports: [RouterOutlet, ToastContainerComponent, ConfirmDialog],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  protected confirmDialog = inject(ConfirmDialogService);
+
+  protected onDialogResult(confirmed: boolean): void {
+    this.confirmDialog.close(confirmed);
+  }
+
+  protected onDialogClosed(): void {
+    // Optional: aditional actions on close
+  }
+}
