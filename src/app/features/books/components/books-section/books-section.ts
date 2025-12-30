0361972d3cfd7263cd8c2fe3dabfc80b2cc68255
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-12-30 11:54:26 +0100

    A - dark mode styles and a new loading spinner for home-book-related components.

--- src/app/features/books/components/books-section/books-section.ts
+++ src/app/features/books/components/books-section/books-section.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { IBook } from '../../models/book.interface';
 import { BookList } from '../book-list/book-list';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'books-section-component',
-  imports: [BookList],
+  imports: [BookList, TitleCasePipe],
   templateUrl: './books-section.html',
   styles: ``,
 })
